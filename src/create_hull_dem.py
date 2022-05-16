cdfa688e08228fb1640d67252a5684194fb77953
Andy Carter <andy.carter@noaa.gov>
2022-05-16 17:21:00 -0500

    Adding inital scripts - 2022.05.16

---
+++ src/create_hull_dem.py
@@ -0,0 +1,248 @@
+# From the bridge hulls and the requested entwine point cloud las files,
+# build a bridge deck dem for each polygon.
+#
+# Created by: Andy Carter, PE
+# Created - 2022.05.16
+# Last revised - 2022.05.16
+#
+# tx-bridge - fourth processing script
+# Uses the 'pdal' conda environment
+
+
+# ************************************************************
+import argparse
+import geopandas as gpd
+import json
+import pdal
+import rioxarray as rxr
+import os
+import tqdm
+
+import time
+import datetime
+# ************************************************************
+
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist" % arg)
+    else:
+        # File exists so return the directory
+        return arg
+        return open(arg, 'r')  # return an open file handle
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+# ````````````````````````````````````````````````````````
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+# ````````````````````````````````````````````````````````
+
+
+# --------------------------------------------------------
+def fn_create_hull_dems(str_bridge_polygons_path,str_output_dir,flt_dem_resolution,b_is_feet):
+    
+    """
+    Determine the major axis line from the bridge hull polygons and the
+    OpenStreetMap lines.
+
+    Args:
+        str_bridge_polygons_path: path convex hull polygons
+        str_output_dir: where to write the road deck dems
+        flt_dem_resolution: resolution of the DEM to create
+        b_is_feet: T/F create data in vertical feet
+
+    Returns:
+        nothing
+    """
+    
+    print(" ")
+    print("+=================================================================+")
+    print("|      CREATE DEM FOR BRIDGE DECKS FROM LAS AND POLYGON HULL      |")
+    print("|                Created by Andy Carter, PE of                    |")
+    print("|             Center for Water and the Environment                |")
+    print("|                 University of Texas at Austin                   |")
+    print("+-----------------------------------------------------------------+")
+
+    print("  ---(i) INPUT CONVEX HULL SHAPEFILE PATH: " + str_bridge_polygons_path)
+    print("  ---(o) OUTPUT DIRECTORY: " + str_output_dir)
+    print("  ---[r]   Optional: RESOLUTION OF DEMS TO CREATE: " + str(flt_dem_resolution) )
+    print("  ---[v]   Optional: VERTICAL DATA IN FEET: " + str(b_is_feet) )
+    print("===================================================================")
+    
+    # create the output directory if it does not exist
+    os.makedirs(str_output_dir, exist_ok=True)
+
+    # read the bridge polygons
+    gdf_bridge_ar = gpd.read_file(str_bridge_polygons_path)
+    
+    # TODO - Multiprocessing of this loop for speed - 20220516
+    for index, row in tqdm.tqdm(gdf_bridge_ar.iterrows(),
+                            total = gdf_bridge_ar.shape[0],
+                            desc='Create DEMs',
+                            bar_format = "{desc}:({n_fmt}/{total_fmt})|{bar}| {percentage:.1f}%",
+                            ncols=65):
+    
+        list_clouds = eval(gdf_bridge_ar.iloc[index]['las_paths'])
+        
+        # create a file name
+        str_bridge_dem = str_output_dir + '\\' + str(index) + '_bridge_deck_dem.tif'
+        
+        # create a pdal pipeline dictionaries - specific tile request
+    
+        # pdal readers for a single tile
+        dict_read_single_las = {
+            "type":"readers.las",
+            "filename":list_clouds[0],
+            "tag": "merge_las"
+        }
+    
+        # pdal writers to create DEM
+        dict_create = {
+                "filename": str_bridge_dem,
+                "gdalopts": "tiled=yes,     compress=deflate",
+                "inputs": [ "merge_las" ],
+                "nodata": -9999,
+                "output_type": "idw",
+                "resolution": flt_dem_resolution,
+                "type": "writers.gdal"
+            }
+    
+        # pdal merge multiple LAS tiles
+        dict_merge = {
+                "type" : "filters.merge",
+                "tag": "merge_las"
+        }
+        
+        list_pipeline = []
+        
+        # create a pdal pipeline for just one tile
+        if len(list_clouds) == 1:
+            list_pipeline = [dict_read_single_las, dict_create]
+    
+        # create pdal pipeline to merge multiple tiles
+        if len(list_clouds) > 1:
+            for i in list_clouds:
+                # create pdal readers for each tile
+                dict_current = {
+                    "type":"readers.las",
+                    "filename":i
+                }
+                list_pipeline.append(dict_current)
+            list_pipeline.append(dict_merge)
+            list_pipeline.append(dict_create)
+        
+        # create pipeline dictionary
+        # pdal pipelines are dictionaries with a list of dictionaries
+        dict_pipeline_merge = {
+            "pipeline": list_pipeline
+        }
+        
+        #execute the pdal pipeline
+        pipeline = pdal.Pipeline(json.dumps(dict_pipeline_merge))
+        n_points = pipeline.execute()
+        
+        if n_points > 0:
+            #bridges were found
+    
+            # read the DEM as a "Rioxarray"
+            bridge_dem = rxr.open_rasterio(str_bridge_dem, masked=True).squeeze()
+    
+            # get a geodataframe of just one row
+            gdf_singlerow = gdf_bridge_ar.iloc[[index],:]
+    
+            # clip the DEM from points to the polygon limits
+            clipped = bridge_dem.rio.clip(gdf_singlerow.geometry,
+                                          gdf_singlerow.geometry.crs,
+                                          drop=True, invert=False)
+    
+            # fill in the missing pixels
+            filled = clipped.rio.interpolate_na()
+    
+            # clip the filled-in data to the polygon boundary
+            clipped2 = filled.rio.clip(gdf_singlerow.geometry,
+                                       gdf_bridge_ar.geometry.crs,
+                                       drop=True, invert=False)
+    
+            # convert vertical values to meters
+            if b_is_feet:
+                # scale the raster from meters to feet
+                clipped2 = clipped2 * 3.28084
+    
+                # write out the raster
+                bridge_dem_out = str_bridge_dem[:-4] + '_vert_ft.tif'
+                clipped2.rio.to_raster(bridge_dem_out, compress='LZW', dtype="float32")
+                
+            else:
+                # write out the raster
+                bridge_dem_out = str_bridge_dem[:-4] + '_vert_m.tif'
+                clipped2.rio.to_raster(bridge_dem_out, compress='LZW', dtype="float32")
+                
+        # TODO - Need to delete the first raster created - prior to clipping - 2022.05.16
+# --------------------------------------------------------
+    
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == '__main__':
+
+    flt_start_run = time.time()
+    
+    parser = argparse.ArgumentParser(description='======== BUILD BRIDGE DECK DEMS FROM BRIDGE HULL POLYGONS =========')
+
+    
+    parser.add_argument('-i',
+                        dest = "str_bridge_polygons_path",
+                        help=r'REQUIRED: path to bridge hull polygon shapefile Example: C:\test\cloud_harvest\02_shapefile_of_hulls\class_17_ar_3857.shp',
+                        required=True,
+                        metavar='FILE',
+                        type=lambda x: is_valid_file(parser, x))
+
+    parser.add_argument('-o',
+                        dest = "str_output_dir",
+                        help=r'REQUIRED: directory to write deck dems: Example: C:\test\cloud_harvest\05_bridge_deck_dems',
+                        required=True,
+                        metavar='DIR',
+                        type=str)
+    
+    parser.add_argument('-r',
+                        dest = "flt_dem_resolution",
+                        help='OPTIONAL: resolution of the bridge deck dem: Default=0.3',
+                        required=False,
+                        default=0.3,
+                        metavar='FLOAT',
+                        type=float)
+    
+    parser.add_argument('-v',
+                        dest = "b_is_feet",
+                        help='OPTIONAL: create vertical data in feet: Default=True',
+                        required=False,
+                        default=True,
+                        metavar='T/F',
+                        type=str2bool)
+    
+    args = vars(parser.parse_args())
+    
+    str_bridge_polygons_path = args['str_bridge_polygons_path']
+    str_output_dir = args['str_output_dir']
+    flt_dem_resolution = args['flt_dem_resolution']
+    b_is_feet = args['b_is_feet']
+    
+    fn_create_hull_dems(str_bridge_polygons_path,
+                        str_output_dir,
+                        flt_dem_resolution,
+                        b_is_feet)
+    
+    flt_end_run = time.time()
+    flt_time_pass = (flt_end_run - flt_start_run) // 1
+    time_pass = datetime.timedelta(seconds=flt_time_pass)
+    
+    print('Compute Time: ' + str(time_pass))
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ src/determine_major_axis.py
@@ -0,0 +1,282 @@
+# From the OpenStreetMap linework and the polygons of the bridges, determine
+# the longest line that crosses the bridge polygons.  This is to determine the
+# 'major axis' line for each bridge.
+#
+# Created by: Andy Carter, PE
+# Created - 2022.05.16
+# Last revised - 2022.05.16
+#
+# tx-bridge - fourth processing script
+# Uses the 'pdal' conda environment
+
+
+# ************************************************************
+import argparse
+import geopandas as gpd
+import pandas as pd
+
+import os
+
+from shapely.geometry import LineString
+
+import time
+import datetime
+
+import tqdm
+from time import sleep
+# ************************************************************
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist" % arg)
+    else:
+        # File exists so return the directory
+        return arg
+        return open(arg, 'r')  # return an open file handle
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def fn_get_major_axis_for_polygon(shp_bridge_ar_fn, flt_bridge_buffer_fn, gdf_trans):
+
+    """
+    Last revised - 20220418
+    
+    Get the major axis of the polygon object 'bridge' from an input line vector dataset 'transportation'
+
+    Args:
+
+        shp_bridge_ar_fn: polygon shape of the bridge
+        flt_bridge_buffer_fn: distance to buffer the bridge polygon
+        gdf_trans: Geodataframe of linear transportation with crs set to same as shp_bridge_ar_fn
+        
+    Returns:
+
+        shp_major_axis: shapely linestring of the major axis
+    """
+    
+    # bridge polygon to Linestring
+    shp_bridge_ln = LineString(list(shp_bridge_ar_fn.exterior.coords))
+
+    # buffer the shape - to get some distance beyond the abutments
+    shp_bridge_buffer_ar = shp_bridge_ar_fn.buffer(flt_bridge_buffer_fn)
+
+    # create a geodataframe of the buffered polygon
+    gdf_current_bridge_buffer = gpd.GeoDataFrame(index=[0], crs = gdf_trans.crs, geometry = [shp_bridge_buffer_ar])
+
+    # compute the area of the buffered polygon
+    gdf_current_bridge_buffer['area'] = gdf_current_bridge_buffer['geometry'].area
+
+    ## determine the trasportation linear features that clip to boundary polygon
+
+    # clip the roads/rail to the bridge lines
+    gdf_current_bridge_trans_intersect = gpd.overlay(gdf_trans, gdf_current_bridge_buffer, how='intersection')
+
+    # compute a length field for each intersected and clipped line
+    gdf_current_bridge_trans_intersect['length'] = gdf_current_bridge_trans_intersect['geometry'].length
+
+    ## pull only the intersecting lines that could be a 'major axis' [longer than the square root of the area]
+    list_possible_major_ln = []
+
+    for index, row in gdf_current_bridge_trans_intersect.iterrows():
+        flt_minor_axis = row['area'] / row['length']
+        if flt_minor_axis < row['length']:
+            list_possible_major_ln.append(index)
+
+    if len(list_possible_major_ln) > 0:
+        # create a new gdf of possible major axis
+        gdf_possible_major_axis_ln = gdf_current_bridge_trans_intersect.iloc[list_possible_major_ln]
+
+        # resest the dataframe index
+        gdf_possible_major_axis_ln = gdf_possible_major_axis_ln.reset_index()
+
+        list_long_axis_id_cross_twice = []
+
+        # add lines that cross the at least twice
+        for index, row in gdf_possible_major_axis_ln.iterrows():
+
+            # get shape of current row (transportation line)
+            shp_transport_ln = gdf_possible_major_axis_ln.iloc[index]['geometry']
+
+            # intersection of the buffered bridge deck exterior and 'long axis' line
+            shp_intersect_pnt = shp_transport_ln.intersection(shp_bridge_ln)
+
+            if shp_intersect_pnt.geom_type == "MultiPoint":
+                #long axis crosses the buffered boundary more than once
+                list_long_axis_id_cross_twice.append(index)
+
+        # crete a geodataframe of just the indecies in list_long_axis_id_cross_twice
+        if len(list_long_axis_id_cross_twice) > 0:
+            gdf_possible_major_axis_cross_twice_ln = gdf_possible_major_axis_ln.iloc[list_long_axis_id_cross_twice]
+
+            # TODO - do we want the line nearest the polygon centroid? - 20220418
+            # get the longest line of the remaining lines
+            int_rowid_longest = gdf_possible_major_axis_cross_twice_ln['length'].idxmax()
+
+            shp_major_axis = gdf_possible_major_axis_cross_twice_ln.iloc[int_rowid_longest]['geometry']
+
+            return(shp_major_axis)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# --------------------------------------------------------
+def fn_determine_major_axis(str_bridge_polygons_path,str_trans_line_path,str_output_dir,flt_buffer_hull):
+    
+    """
+    Determine the major axis line from the bridge hull polygons and the
+    OpenStreetMap lines.
+
+    Args:
+        str_bridge_polygons_path: path convex hull polygons
+        str_trans_line_path: path to the OpenStreetMap transporation lines
+        str_output_dir: where to write the shapefile of the hull lines
+        flt_buffer_hull: distance to extend the major axis beyond hull - OSM linework units = AOI units
+
+    Returns:
+        geodataframe of major axis
+    """
+    
+    print(" ")
+    print("+=================================================================+")
+    print("|              DETERMINE MAJOR-AXIS FOR BRIDGE HULLS              |")
+    print("|                Created by Andy Carter, PE of                    |")
+    print("|             Center for Water and the Environment                |")
+    print("|                 University of Texas at Austin                   |")
+    print("+-----------------------------------------------------------------+")
+
+    print("  ---(b) INPUT CONVEX HULL SHAPEFILE PATH: " + str_bridge_polygons_path)
+    print("  ---(t) TRANSPORTATION SHAPEFILE PATH: " + str_trans_line_path)
+    print("  ---(o) OUTPUT DIRECTORY: " + str_output_dir)
+    print("  ---[x]   Optional: AXIS BUFFER DISTANCE: " + str(flt_buffer_hull) )
+    print("===================================================================")
+    
+    # create the output directory if it does not exist
+    os.makedirs(str_output_dir, exist_ok=True)
+    
+    # read the bridge polygons
+    gdf_bridge_ar = gpd.read_file(str_bridge_polygons_path)
+    
+    # read the transporation linework
+    gdf_trans = gpd.read_file(str_trans_line_path)
+    
+    # set the crs of the gdf_bridge_ar
+    gdf_bridge_ar = gdf_bridge_ar.to_crs(gdf_trans.crs)
+    
+    # get the major axis for each polygon in the provided hulls
+    list_major_axis_shp = []
+    
+    
+    for index, row in tqdm.tqdm(gdf_bridge_ar.iterrows(),
+                                total = gdf_bridge_ar.shape[0],
+                                desc='Determine axis',
+                                bar_format = "{desc}:({n_fmt}/{total_fmt})|{bar}| {percentage:.1f}%",
+                                ncols=65):
+        shp_bridge_ar = row['geometry']
+        sleep(0)
+        
+        shp_major_axis = fn_get_major_axis_for_polygon(shp_bridge_ar, flt_buffer_hull, gdf_trans)
+        list_major_axis_shp.append(shp_major_axis)
+
+    # add the major axis linestrings to the geodataframe
+    gdf_bridge_ar['mjr_axis'] = list_major_axis_shp
+    
+    # add new coloumns
+    gdf_bridge_ar["hull_len"] = None
+    gdf_bridge_ar["avg_width"] = None
+    
+    # computing the bridge length and width
+    for index,row in gdf_bridge_ar.iterrows():
+        if not (pd.isnull(row['mjr_axis'])):
+            # Geodataframe has a valid linestring for mjr axis
+            shp_mjr_axis_ln = row['mjr_axis']
+            shp_bridge_ar = row['geometry']
+            
+            # clip major axis line to bridge hull
+            # TODO - what if there is no intersection? 20220421
+            shp_mjr_axis_clip = shp_mjr_axis_ln.intersection(shp_bridge_ar)
+            
+            flt_bridge_area = shp_bridge_ar.area
+            
+            flt_mjr_axis_length = shp_mjr_axis_clip.length
+            flt_avg_width = flt_bridge_area / flt_mjr_axis_length
+            
+            gdf_bridge_ar.iat[index, gdf_bridge_ar.columns.get_loc('hull_len')] = flt_mjr_axis_length
+            gdf_bridge_ar.iat[index, gdf_bridge_ar.columns.get_loc('avg_width')] = flt_avg_width
+    
+    # copy the geodataframe
+    gdf_bridge_mjr_axis_ln = gdf_bridge_ar
+    
+    # set the active geometry to the mjr_axis
+    gdf_bridge_mjr_axis_ln = gdf_bridge_mjr_axis_ln.set_geometry("mjr_axis")
+    
+    # get row in gdf_bridge_mjr_axis_ln where 'mjr_axis' is null
+    nan_mjr_axis_rows = gdf_bridge_mjr_axis_ln[gdf_bridge_mjr_axis_ln['mjr_axis'].isnull()]
+    
+    # remove the null rows from the dataframe
+    gdf_bridge_mjr_axis_ln = gdf_bridge_mjr_axis_ln.drop(nan_mjr_axis_rows.index)
+    
+    # delete the las_paths
+    # TODO -2022.05.16 stringify instead
+    del gdf_bridge_mjr_axis_ln['las_paths']
+    del gdf_bridge_mjr_axis_ln['geometry']
+    
+    str_file_shp_to_write = str_output_dir + '\\' +'mjr_axis_ln.shp'
+    gdf_bridge_mjr_axis_ln.to_file(str_file_shp_to_write)
+
+# --------------------------------------------------------
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == '__main__':
+
+    flt_start_run = time.time()
+    
+    parser = argparse.ArgumentParser(description='============== DETERMINE MAJOR-AXIS FOR BRIDGE HULLS ==============')
+    
+    parser.add_argument('-b',
+                        dest = "str_bridge_polygons_path",
+                        help=r'REQUIRED: path to bridge hull polygon shapefile Example: C:\test\cloud_harvest\02_shapefile_of_hulls\class_17_ar_3857.shp',
+                        required=True,
+                        metavar='FILE',
+                        type=lambda x: is_valid_file(parser, x))
+
+    parser.add_argument('-t',
+                        dest = "str_trans_line_path",
+                        help=r'REQUIRED: path to transporation line shapefile: Example: C:\test\cloud_harvest\03_osm_trans_lines\osm_trans_ln.shp',
+                        required=True,
+                        metavar='FILE',
+                        type=lambda x: is_valid_file(parser, x))
+    
+    parser.add_argument('-o',
+                        dest = "str_output_dir",
+                        help=r'REQUIRED: directory to write line shapefile: Example: C:\test\cloud_harvest\04_major_axis_lines',
+                        required=True,
+                        metavar='DIR',
+                        type=str)
+    
+    parser.add_argument('-x',
+                        dest = "flt_buffer_hull",
+                        help='OPTIONAL: distance to extend major axis beyond hull: Default=20',
+                        required=False,
+                        default=20,
+                        metavar='FLOAT',
+                        type=float)
+    
+    args = vars(parser.parse_args())
+    
+    str_bridge_polygons_path = args['str_bridge_polygons_path']
+    str_trans_line_path = args['str_trans_line_path']
+    str_output_dir = args['str_output_dir']
+    flt_buffer_hull = args['flt_buffer_hull']
+    
+    fn_determine_major_axis(str_bridge_polygons_path,
+                            str_trans_line_path,
+                            str_output_dir,
+                            flt_buffer_hull)
+    
+    flt_end_run = time.time()
+    flt_time_pass = (flt_end_run - flt_start_run) // 1
+    time_pass = datetime.timedelta(seconds=flt_time_pass)
+    
+    print('Compute Time: ' + str(time_pass))
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
